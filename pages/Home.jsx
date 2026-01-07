72338d01d557b0d80ad8942404e6eeb6b6e0a104
Fede <204039362+FunesFede@users.noreply.github.com>
2026-01-07 02:09:25 -0300

    add padding

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -19,7 +19,7 @@ export default function Main() {
 	return (
 		<>
 			<div className='py-4 bg-background flex flex-col flex-grow min-h-screen'>
-				<div className='container mx-auto'>
+				<div className='container mx-auto px-4'>
 					<h3 className='text-start mb-6 text-2xl font-semibold'></h3>
 
 					<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4'>
